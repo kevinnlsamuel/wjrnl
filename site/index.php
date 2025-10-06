968c3cf824a050ef795bcad2887fceb660f6eb6e
kevinnls <developer@kevinnlsamuel.com>
2025-10-07 03:10:38 +0530

    install tailwindcss dependencies + set up

--- site/index.php
+++ site/index.php
@@ -1,6 +1,12 @@
 <?php
 include 'partials/head.php'
 ?>
+<body class="dark:bg-black bg-white">
+<h1 class="my-heading my-4 hover:text-pink-400 dark:text-white font-bold">
 this is the index
 asdfasdf
+</h1>
+
+<p class="uppercase">Hello!</p>
+</body>
 
